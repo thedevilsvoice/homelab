802031100172a86128b4931257b3574ca5137b42
FrankTheTank <franklin@bitsmasher.net>
2020-11-02 15:38:07 -0500

    test ngfw ansible

--- ansible/ansible_setup.sh
+++ ansible/ansible_setup.sh
@@ -14,3 +14,8 @@ ssh-add ~/.ssh/id_rsa
 ansible cluster -m ping --become -e 'ansible_python_interpreter=/usr/bin/python3'
 
 ansible-playbook playbook.yml -i hosts --become -e 'ansible_python_interpreter=/usr/bin/python3'
+
+
+# Firewall
+# Examples: https://github.com/PaloAltoNetworks/ansible-pan/tree/master/examples
+ansible-playbook firewall.yml -i firewalls
